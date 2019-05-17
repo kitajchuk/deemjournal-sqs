69b6418ac5dabe248210dfe7c29258a0ae31a54b
kitajchuk <kitajchuk@gmail.com>
2019-05-16 19:14:57 -0700

    story text stylings

--- source/js/class/components/Story.js
+++ source/js/class/components/Story.js
@@ -15,6 +15,8 @@ class Story {
     constructor ( element, data ) {
         this.element = element;
         this.coverRow = this.element.find( ".sqs-layout > .sqs-row > .col > .sqs-row:first-child" );
+        this.bolders = this.element.find( ".sqs-layout > .sqs-row > .col .sqs-block-html .sqs-block-content > p" );
+        this.numerics = this.element.find( ".sqs-layout > .sqs-row > .col .sqs-block-html .sqs-block-content > p > em" );
         this.data = data;
 
         this.init();
@@ -33,6 +35,29 @@ class Story {
         if ( this.coverRow.length ) {
             this.coverRow.find( ".col > .sqs-block-html" ).parent().addClass( "text-col" );
             this.coverRow.find( ".col > .sqs-block-image" ).parent().addClass( "image-col" );
+        }
+
+        if ( this.bolders.length ) {
+            this.bolders.forEach(( el, i ) => {
+                const elem = this.bolders.eq( i );
+                const strong = elem.find( "strong" );
+                const em = elem.find( "em" );
+
+                if ( !em.length && strong.length ) {
+                    elem.addClass( "p--boldline" );
+                }
+            });
+        }
+
+        if ( this.numerics.length ) {
+            this.numerics.forEach(( el, i ) => {
+                const elem = this.numerics.eq( i );
+                const number = parseInt( el.innerText, 10 );
+
+                if ( Number.isInteger( number ) ) {
+                    elem.addClass( "ss" );
+                }
+            });
         }
     }
 
