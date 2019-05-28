5cf7ea781b474ef42fa4a6603000a7cdfac49e73
kitajchuk <kitajchuk@gmail.com>
2019-05-28 15:34:14 -0700

    shop grids

--- source/js/views/shop.js
+++ source/js/views/shop.js
@@ -1,15 +1,58 @@
 export default ( instance ) => {
-    const items = instance.data.items;
+    const items = instance.data.items.filter(( item ) => {
+        return !item.isSubscribable;
+    });
+    const subItem = instance.data.items.find(( item ) => {
+        return item.isSubscribable;
+    });
+    const isGridView = (items.length >= 3);
+    const getGridView = () => {
+        return `<div class="grid js-shop-grid">${items.map(( item ) => {
+            return `
+                <div class="grid__item">
+                    <a class="grid__link" href="${item.fullUrl}">
+                        <img class="grid__image image js-lazy-image" data-img-src="${item.assetUrl}" data-variants="${item.systemDataVariants}" data-original-size="${item.originalSize}" />
+                        <div class="grid__title">
+                            <div>${item.title}</div>
+                            <div>${item.structuredContent.variants[ 0 ].priceMoney.value}</div>
+                        </div>
+                    </a>
+                </div>
+            `;
 
-    return `<div class="grid js-shop-grid">${items.map(( item ) => {
-        return `
-            <div class="grid__item">
-                <a class="grid__link" href="${item.fullUrl}">
-                    <img class="grid__image image js-lazy-image" data-img-src="${item.assetUrl}" data-variants="${item.systemDataVariants}" data-original-size="${item.originalSize}" />
-                    <div class="grid__title">${item.title}</div>
-                </a>
+        }).join( "" )}</div>`;
+    };
+    const getStackView = () => {
+        return `${items.map(( item ) => {
+            const isIssue = (item.tags[ 0 ] === "Issue");
+
+            return `
+                <div class="stack">
+                    <div class="stack__wrap">
+                        <img class="stack__image image js-lazy-image" data-img-src="${item.assetUrl}" data-variants="${item.systemDataVariants}" data-original-size="${item.originalSize}" />
+                        <div class="stack__info">
+                            <h4>${item.excerpt || item.title}</h4>
+                            <a class="h6" href="${item.fullUrl}">${isIssue ? `View Issue` : `Learn More`}</a>
+                        </div>
+                    </div>
+                </div>
+            `;
+
+        }).join( "" )}`;
+    };
+
+    return `
+        ${isGridView ? getGridView() : getStackView()}
+        ${subItem ? `
+            <div class="stack stack--sub">
+                <div class="stack__wrap">
+                    <img class="stack__image image js-lazy-image" data-img-src="${subItem.assetUrl}" data-variants="${subItem.systemDataVariants}" data-original-size="${subItem.originalSize}" />
+                    <div class="stack__info">
+                        <h4>${subItem.title}</h4>
+                        <a class="h6" href="${subItem.fullUrl}">${subItem.excerpt}</a>
+                    </div>
+                </div>
             </div>
-        `;
-
-    }).join( "" )}</div>`;
+        ` : ``}
+    `;
 };
