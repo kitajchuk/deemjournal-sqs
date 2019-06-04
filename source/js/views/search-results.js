0c90afd2968ce6dc56df7a52f150d0ed5dc63227
kitajchuk <kitajchuk@gmail.com>
2019-06-04 10:55:14 -0700

    filter out THIS story in search results if on a story page

--- source/js/views/search-results.js
+++ source/js/views/search-results.js
@@ -1,8 +1,13 @@
+import * as core from "../core";
+
+
+
 export default ( json ) => {
     const items = json.items;
+    const story = core.dom.main.find( ".js-story" );
 
     return items.length ? `<div class="grid js-search-grid">${items.map(( item ) => {
-        return `
+        return (story.length && story.data().itemId !== item.id) ? `
             <div class="grid__item">
                 <a class="grid__link" href="${item.itemUrl || item.fullUrl}">
                     <img class="grid__image image js-lazy-image" data-img-src="${item.imageUrl || item.assetUrl}" />
@@ -11,7 +16,7 @@ export default ( json ) => {
                     </div>
                 </a>
             </div>
-        `;
+        `: ``;
 
     }).join( "" )}</div>` : `<div class="grid js-search-grid"><div class="grid__item"><p class="-grey">no results</p></div></div>`;
 };
