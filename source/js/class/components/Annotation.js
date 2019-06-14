856de067480333f3eec12ae9b5490707ae6c101a
kitajchuk <kitajchuk@gmail.com>
2019-06-14 10:36:29 -0700

    vertical spacing. quick tag overlay.

--- source/js/class/components/Annotation.js
+++ source/js/class/components/Annotation.js
@@ -18,6 +18,10 @@ class Annotation {
         this.parent = this.annotation.closest( ".sqs-row" );
         this.text = this.parent.find( ".col:nth-child(2) .sqs-block-content > p, .col:nth-child(2) .sqs-block-content > blockquote" );
         this.media = this.parent.find( ".js-media" );
+        this.prevSpace = this.parent.prev( ".sqs-block-spacer" );
+        this.nextSpace = this.parent.next( ".sqs-block-spacer" );
+        this.prevText = this.prevSpace.prev( ".sqs-block-html" );
+        this.nextText = this.nextSpace.next( ".sqs-block-html" );
         this.data = data;
         this.i = i;
 
@@ -27,6 +31,14 @@ class Annotation {
 
     init () {
         this.parent[ 0 ].innerHTML = annotationView( this );
+
+        if ( this.prevText.length ) {
+            this.prevSpace.addClass( "spacer-text" );
+        }
+
+        if ( this.nextText.length ) {
+            this.nextSpace.addClass( "spacer-text" );
+        }
     }
 
 
--- source/js/class/components/Story.js
+++ source/js/class/components/Story.js
@@ -1,5 +1,8 @@
-// import * as core from "../../core";
-// import $ from "properjs-hobo";
+import * as core from "../../core";
+import $ from "properjs-hobo";
+import viewSearchResults from "../../views/search-results";
+import ImageController from "../controllers/ImageController";
+
 
 
 /**
@@ -20,6 +23,7 @@ class Story {
         this.listings = this.element.find( ".sqs-layout > .sqs-row > .col .sqs-block-html .sqs-block-content > ul, .sqs-layout > .sqs-row > .col .sqs-block-html .sqs-block-content > ol" );
         this.summary = this.element.find( ".js-summary-v2" );
         this.capillary = this.element.find( ".sqs-layout > .sqs-row > .col .sqs-block-gallery + .sqs-block-html > .sqs-block-content > blockquote:first-child " );
+        this.tagcloud = this.element.find( ".js-tagcloud" );
         this.data = data;
 
         this.init();
@@ -28,66 +32,164 @@ class Story {
 
     init () {
         if ( this.coverRow.length ) {
-            this.coverRow.find( ".col > .sqs-block-html" ).parent().addClass( "text-col" );
-            this.coverRow.find( ".col > .sqs-block-image" ).parent().addClass( "image-col" );
-            this.element.addClass( "story--alt" );
+            this.initCover();
         }
 
         if ( this.bolders.length ) {
-            this.bolders.forEach(( el, i ) => {
-                const elem = this.bolders.eq( i );
-                const strong = elem.find( "strong" );
-                const em = elem.find( "em" );
-
-                if ( !em.length && strong.length ) {
-                    elem.addClass( "p--boldline" );
-                }
-            });
+            this.initBolders();
         }
 
         if ( this.numerics.length ) {
-            this.numerics.forEach(( el, i ) => {
-                const elem = this.numerics.eq( i );
-                const number = parseInt( el.innerText, 10 );
-
-                if ( Number.isInteger( number ) ) {
-                    elem.addClass( "ss" );
-                }
-            });
+            this.initNumerics();
         }
 
         if ( this.listings.length ) {
-            this.listings.forEach(( el, i ) => {
-                const elem = this.listings.eq( i );
-                const prev = elem.prev( "p" );
-
-                if ( prev.length ) {
-                    prev.addClass( "p--listings" );
-                }
-            });
+            this.initListings();
         }
 
         if ( this.capillary.length ) {
-            this.capillary.forEach(( el, i ) => {
-                const gallery = this.capillary
-                    .eq( i )
-                    .closest( ".sqs-block-html" )
-                    .prev( ".sqs-block-gallery" );
-
-                gallery
-                    .addClass( "is-capillary" )
-                    .find( ".sqs-block-content" )
-                    .append( el );
-            });
+            this.initCapillaries();
         }
 
         if ( this.summary.length ) {
             this.summary.find( `.js-summary-v2-item--${this.data.itemId}` ).remove();
         }
+
+        if ( this.tagcloud.length ) {
+            this.initTagcloud();
+        }
     }
 
 
-    destroy () {}
+    initCover () {
+        this.coverRow.find( ".col > .sqs-block-html" ).parent().addClass( "text-col" );
+        this.coverRow.find( ".col > .sqs-block-image" ).parent().addClass( "image-col" );
+        this.element.addClass( "story--alt" );
+    }
+
+
+    initBolders () {
+        this.bolders.forEach(( el, i ) => {
+            const elem = this.bolders.eq( i );
+            const strong = elem.find( "strong" );
+            const em = elem.find( "em" );
+
+            if ( !em.length && strong.length ) {
+                elem.addClass( "p--boldline" );
+            }
+        });
+    }
+
+
+    initNumerics () {
+        this.numerics.forEach(( el, i ) => {
+            const elem = this.numerics.eq( i );
+            const number = parseInt( el.innerText, 10 );
+
+            if ( Number.isInteger( number ) ) {
+                elem.addClass( "ss" );
+            }
+        });
+    }
+
+
+    initListings () {
+        this.listings.forEach(( el, i ) => {
+            const elem = this.listings.eq( i );
+            const prev = elem.prev( "p" );
+
+            if ( prev.length ) {
+                prev.addClass( "p--listings" );
+            }
+        });
+    }
+
+
+    initCapillaries () {
+        this.capillary.forEach(( el, i ) => {
+            const gallery = this.capillary
+                .eq( i )
+                .closest( ".sqs-block-html" )
+                .prev( ".sqs-block-gallery" );
+
+            gallery
+                .addClass( "is-capillary" )
+                .find( ".sqs-block-content" )
+                .append( el );
+        });
+    }
+
+
+    initTagcloud () {
+        this.tagcloudQuery = this.tagcloud.next( ".js-tagcloud-query" );
+        this.tagcloudLabel = this.tagcloudQuery.find( ".js-tagcloud-field" );
+        this.tagcloudLoader = this.tagcloudQuery.find( ".js-tagcloud-loader" );
+        this.tagcloudDisplay = this.tagcloudQuery.find( ".js-tagcloud-display" );
+        this.tagcloudClose = this.tagcloudQuery.find( ".js-tagcloud-close" );
+        this.tagcloud.on( "click", ".js-tagcloud-link", ( e ) => {
+            const target = $( e.target );
+            const elem = target.is( ".js-tagcloud-link" ) ? target : target.parent();
+            const data = elem.data();
+
+            this.tagcloudQuery.addClass( "is-active" );
+            this.tagcloudLoader.addClass( "is-active" );
+            this.tagcloudLabel[ 0 ].placeholder = data.tag;
+            this.tagcloudLabel[ 0 ].disabled = true;
+
+            this.fetchStories( data.tag ).then( this.handleStories.bind( this ) );
+        });
+
+        this.tagcloudClose.on( "click", () => {
+            this.tagcloudQuery.removeClass( "is-active" );
+
+            setTimeout( this.handleTimeout.bind( this ), core.config.defaultDuration );
+        });
+    }
+
+
+    handleTimeout () {
+        this.tagcloudLabel[ 0 ].placeholder = "";
+        this.tagcloudDisplay[ 0 ].innerHTML = "";
+        this.imageController.destroy();
+        this.imageController = null;
+    }
+
+
+    handleStories ( json ) {
+        this.tagcloudLoader.removeClass( "is-active" );
+        this.tagcloudDisplay[ 0 ].innerHTML = viewSearchResults( json );
+        this.imageController = new ImageController( this.tagcloudDisplay.find( core.config.lazyImageSelector ) );
+        this.imageController.on( "preloaded", () => {
+            this.tagcloudDisplay.find( ".js-search-grid" ).addClass( "is-active" );
+        });
+    }
+
+
+    fetchStories ( tag ) {
+        return new Promise(( resolve, reject ) => {
+            $.ajax({
+                url: "/stories/",
+                method: "GET",
+                dataType: "json",
+                data: {
+                    format: "json",
+                    tag,
+                }
+            }).then(( response ) => {
+                resolve( response );
+
+            }).catch(( error ) => {
+                reject( error );
+            });
+        });
+    }
+
+
+    destroy () {
+        if ( this.imageController ) {
+            this.imageController.destroy();
+        }
+    }
 }
 
 
